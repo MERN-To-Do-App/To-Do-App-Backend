431d6dda16bf7dcddc938f5caa44004ed9785684
Sahil-Soni1 <sonisahil334@gmail.com>
2022-03-28 21:35:09 +0530

    login 1

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
+
+const passport = require('passport');
 const User = require('../models/user');
 exports.login = function(passport.authenticate('local'), (req, res) => { 
     res.statusCode = 200;                                                                    
     res.json({success: true, status: 'You are successfully logged in'});   
-  })
+  });
